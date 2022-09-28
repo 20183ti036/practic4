d1cfa3ccc9418ab3f3728dacf6c22e325b0a2ea0
Romario Herrera <99297874+20183ti036@users.noreply.github.com>
2022-09-27 23:49:32 -0500

    Add functions
    cache

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', (event) => {
+    console.log("SW si funciona ");
+    const promiseCache = caches.open('cache-v1.1').then((cache) => {
+        return cache.addAll(
+            [
+                '/',
+                '/index.html',
+                '/porfolio/Division.html',
+                '/porfolio/Multiplicacion.html',
+                '/porfolio/Resta.html',
+                '/porfolio/suma.html',
+                '/css/style.css',
+                '/image/KOBE.jpg',
+                '/js/app.js',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
+            ]
+        );
+    })
+    //espera hasta que termine
+    event.waitUntil(promiseCache)
+})
+
+self.addEventListener('fetch', (event) => {
+    const respCache = caches.match(event.request);
+    event.respondWith(respCache);
+})
